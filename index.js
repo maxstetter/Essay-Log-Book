190fad2e2c2ff9afd3f5f52dd2c8e78c041619c8
Max <Max>
2022-03-03 20:07:52 -0700

    made the expression what it was before. Moved it to the bottom of the app.use calls.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const Note = model.Note;
 const app = express()
 const port = process.env.PORT
 
-app.use(express.static(__dirname, ('public')));
 app.use(cors());
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json({}))
+app.use(express.static('public'));
 
 app.put('/essays/:essayId', (req, res)=> {
     Essay.findOneAndUpdate({
